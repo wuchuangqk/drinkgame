6591ba55c999983f8520d42ecaff1447fd70bd9d
吴闯 <wuchuang456@126.com>
2023-07-05 15:13:04 +0800

    滑动条

--- src/pages/danmu/danmu.vue
+++ src/pages/danmu/danmu.vue
@@ -1,77 +1,93 @@
 <template>
   <view class="page-full page">
-    <view class="content" :style="textStyle">{{ content }}</view>
+    <view class="danmu" :style="textStyle">{{ danmu }}</view>
+    <view class="panel">
+      <view>
+        <!-- <input type="range"> -->
+        <!-- <SliderBar /> -->
+        <movable-area class="movable-area">
+          <movable-view class="movable-view" direction="horizontal"></movable-view>
+        </movable-area>
+      </view>
+    </view>
     <view class="v1">
-      <input type="text" v-model="text" class="input" placeholder="请输入">
-      <button @click="play">发送</button>
+      <input type="text" v-model="inputText" class="input" placeholder="请输入">
+      <button @click="changeDanmu">发送</button>
     </view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref, onBeforeMount, reactive, getCurrentInstance, nextTick } from 'vue'
+import { ref, reactive, getCurrentInstance, nextTick } from 'vue'
 import _ from 'lodash'
 import { onReady, onUnload } from "@dcloudio/uni-app";
+import SliderBar from '@/components/slider-bar.vue'
 
-onBeforeMount(() => {
-})
 onReady(async () => {
   query = uni.createSelectorQuery().in(instance);
-  // 获取页面高度
-  query.select('.page').boundingClientRect((data: any) => {
-    pageHeight = data.height
-  }).exec();
+  const pageHeight = await getPageHeight()
   top = pageHeight
-  textStyle.top = pageHeight + 'px'
-  f1()
+  // textStyle.top = pageHeight + 'px'
+  // textRoll()
 })
 onUnload(() => {
   clearInterval(timer)
 })
-let query
-const f1 = async () => {
-  clearInterval(timer)
-  const width = await getWidth()
-  timer = setInterval(() => {
-    top -= speed
-    // console.log(width, top);
 
-    if (top < -width) {
-      top = pageHeight
-    }
-    textStyle.top = top + 'px'
-  }, 10)
-}
-// 重新获取高度
-const getWidth = () => {
-  return new Promise<number>((resolve) => {
-    query.select('.content').boundingClientRect((data: any) => {
-      console.log(data);
-      resolve(data.height)
-    }).exec();
-  })
-}
+let query: any = null,
+  pageHeight = 0,
+  top = 0,
+  timer: any = null
 const instance = getCurrentInstance()
-let pageHeight = 0
-let top = 0
-
-let deg = 0
-const content = ref('这是字')
+// 弹幕文字
+const danmu = ref('这是字')
 const textStyle = reactive({
   color: '#fff',
   fontSize: '80px',
   top: '0',
   transition: 'none',
 })
-const text = ref('')
-const play = () => {
-  content.value = text.value
-  nextTick(() => {
-    f1()
+const inputText = ref('')
+// 文字滚动速度
+const speed = 3
+
+// 文字滚动
+const textRoll = async () => {
+  clearInterval(timer)
+  const textHeight = await getTextHeight()
+  timer = setInterval(() => {
+    top -= speed
+    // 当top小于自身高度时，意味着已经超出屏幕外
+    if (top < -textHeight) {
+      top = pageHeight
+    }
+    textStyle.top = top + 'px'
+  }, 10)
+}
+
+// 获取页面高度
+const getPageHeight = () => {
+  return new Promise<number>((resolve) => {
+    query.select('.page').boundingClientRect((data: any) => {
+      resolve(data.height)
+    }).exec();
   })
 }
-const speed = 3
-let timer
+// 获取文本高度
+const getTextHeight = () => {
+  return new Promise<number>((resolve) => {
+    query.select('.danmu').boundingClientRect((data: any) => {
+      resolve(data.height)
+    }).exec();
+  })
+}
+// 设置新的弹幕
+const changeDanmu = () => {
+  danmu.value = inputText.value
+  nextTick(() => {
+    textRoll()
+  })
+}
 </script>
 
 <style scoped lang="scss">
@@ -80,7 +96,7 @@ let timer
   background-color: rgb(0, 4, 0);
 }
 
-.content {
+.danmu {
   position: absolute;
   left: 50%;
   transform: rotate(90deg);
@@ -100,5 +116,21 @@ let timer
     flex: 1;
     border: 1px solid #fff;
   }
+}
+
+.panel {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 10%;
+  background-color: #140d20;
+}
+
+.movable-area {
+  width: 100%;
+}
+
+.movable-view {
+  background-color: red;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,10 +6,19 @@
     <navigator url="/pages/danmu/danmu">
       <button type="primary">手持弹幕</button>
     </navigator>
+    <view class="test"></view>
   </view>
 </template>
 
 <script setup lang="ts">
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.test {
+  width: 200rpx;
+  height: 200rpx;
+  background-image: -webkit-linear-gradient(-180deg, rgba(255, 0, 255, 0.8) 10%, #f00 80%);
+  // padding-top: 200rpx;
+  // margin-top: -200rpx;
+}
+</style>
