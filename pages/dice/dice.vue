b7baa5d66d222efb28a04196a911f440ee5eab97
吴闯 <wuchuang456@126.com>
2023-08-10 11:38:30 +0800

    1

--- pages/dice/dice.vue
+++ pages/dice/dice.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="page-full page" @touchstart="touchstart" @touchend="touchend">
+  <view class="page-full page" @touchstart="touchstart" @touchmove="touchmove" @touchend="touchend">
     <AppNavbar />
     <view class="v1">
       <view v-for="dice in diceList" :key="dice.x" class="rect"
@@ -7,9 +7,11 @@
         <image v-if="dice.number !== -1" :src="`http://admin.jlwp.vip/jjsq/${dice.number}@2x.png`" class="dice" />
       </view>
     </view>
-    <image src="http://admin.jlwp.vip/jjsq/zhong_ding@2x.png" class="zhong" :style="{ bottom: isOpen ? '45%' : '28%' }" />
-    <view v-show="isOpen" class="dian flex center">总点数：{{ total }}</view>
-    <view class="v2 flex items-center justify-between">
+    <view class="zhong-wrap" :style="{ top: top }">
+      <image src="http://admin.jlwp.vip/jjsq/zhong_ding@2x.png" class="zhong" :class="{ shake }" />
+    </view>
+    <view v-show="showDice" class="dian flex center">总点数：{{ total }}</view>
+    <view v-show="!shake" class="v2 flex items-center justify-between">
       <view>
         <image src="http://admin.jlwp.vip/jjsq/icon_jjds@2x.png" style="width: 123rpx;height: 118rpx;" />
       </view>
@@ -42,23 +44,40 @@ class Dice {
     this.zIndex = zIndex
   }
 }
+const MAX_DIS = 100
 export default {
   components: { AppNavbar },
   data() {
     return {
       diceList: [
-        new Dice('66rpx', '31rpx', 1),
-        new Dice('133rpx', '47rpx', 1),
-        new Dice('199rpx', '46rpx', 1),
-        new Dice('264rpx', '46rpx', 1),
-        new Dice('198rpx', '-2rpx', 0),
-        new Dice('108rpx', '-6rpx', 0),
+        new Dice('56rpx', '31rpx', 1),
+        new Dice('123rpx', '47rpx', 1),
+        new Dice('189rpx', '46rpx', 1),
+        new Dice('254rpx', '46rpx', 1),
+        new Dice('188rpx', '-2rpx', 0),
+        new Dice('98rpx', '-6rpx', 0),
       ],
       total: 0,
-      isOpen: false,
+      showDice: false, // 骰子点数
       isCanOpen: true,
       startY: 0,
+      shake: false, // 晃动动画
+      top: '48%',
+      origin: '', // 盅的原点
+      lastPosition: '', // 记录盅最后一次移动到的位置
+      touchmove: null,
     }
+  },
+  onLoad() {
+    this.touchmove = _.throttle(this.touchmove1, 50)
+  },
+  onReady() {
+    const query = uni.createSelectorQuery().in(this);
+    query.selectAll('.zhong-wrap').boundingClientRect(data => {
+      const statusBarHeight = uni.getSystemInfoSync().statusBarHeight
+      this.lastPosition = this.origin = data[0].top + statusBarHeight
+    }).exec();
+    this.shuffle()
   },
   methods: {
     back() {
@@ -78,26 +97,46 @@ export default {
         }
       })
     },
-    open() {
-      this.shuffle()
-      this.isCanOpen = false
-      this.isOpen = true
-    },
+    // 重新开始
     reStart() {
-      this.isOpen = false
+      // 盅复位
+      this.top = this.origin + 'px'
+      this.showDice = false
+      this.isCanOpen = false
+      this.shake = true
       setTimeout(() => {
+        this.shuffle()
         this.isCanOpen = true
+        this.shake = false
       }, 1000)
     },
     touchstart(e) {
+      if (!this.isCanOpen) return
       const { clientY } = e.changedTouches[0]
       this.startY = clientY
     },
-    touchend(e) {
+    touchmove1(e) {
+      if (!this.isCanOpen) return
+      // 掀开盅时，显示骰子点数
+      this.showDice = true
       const { clientY } = e.changedTouches[0]
-      if (clientY < this.startY) {
-        this.open()
+      // 计算移动距离
+      const dis = this.startY - clientY
+      if (this.lastPosition - dis < this.origin - MAX_DIS) {
+        this.top = this.origin - MAX_DIS + 'px'
+      } else if (this.lastPosition - dis > this.origin) {
+        this.top = this.origin + 'px'
+      } else {
+        this.top = this.lastPosition - dis + 'px'
       }
+      // 盅回到原位，隐藏骰子点数
+      if (parseFloat(this.top) === this.origin) {
+        this.showDice = false
+      }
+    },
+    touchend(e) {
+      if (!this.isCanOpen) return
+      this.lastPosition = parseFloat(this.top)
     }
   }
 }
@@ -118,7 +157,7 @@ export default {
   background: url("http://admin.jlwp.vip/jjsq/zhongdi.png") no-repeat;
   background-size: contain;
   position: absolute;
-  bottom: 24%;
+  top: 63%;
 }
 
 .start {
@@ -162,14 +201,17 @@ export default {
   right: 0;
 }
 
-.zhong {
+.zhong-wrap {
   width: 307rpx;
   height: 381rpx;
   position: absolute;
   left: 50%;
   transform: translateX(-50%);
-  bottom: 28%;
-  transition: all 1s;
+}
+
+.zhong {
+  width: 100%;
+  height: 100%;
 }
 
 .v3 {
@@ -199,5 +241,29 @@ export default {
     top: 10%;
     opacity: 0;
   }
+}
+
+@keyframes shake {
+  0% {
+    transform: rotate(10deg);
+  }
+
+  35% {
+    transform: rotate(-10deg);
+  }
+
+  60% {
+    transform: rotate(10deg);
+  }
+
+  100% {
+    transform: rotate(-10deg);
+  }
+}
+
+.shake {
+  animation: shake 0.6s;
+  animation-iteration-count: 3;
+  animation-direction: alternate;
 }
 </style>
